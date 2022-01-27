c98e285664b7e5c87ea62940b28403a40effb2eb
Marian05 <mariananghel99@gmail.com>
2022-01-27 02:11:52 +0200

    auto push

--- Production/main.js
+++ Production/main.js
@@ -5,20 +5,20 @@ var body = document.body
 script.src = "https://mariananghel05.github.io/TvApp/Production/src/Request.js"
 document.head.appendChild(script)
 
-var script = document.createElement('script')
+var script = document.createElement("script")
 script.src = "https://mariananghel05.github.io/TvApp/Production/src/script.js"
 console.log("script: ",script)
 document.head.appendChild(script)
 
 
-var style = document.createElement('style')
+var style = document.createElement("style")
 style.rel = 'stylesheet'
 style.href = 'https://mariananghel05.github.io/TvApp/Production/src/style.css'
 document.head.appendChild(style)
 
 var http = new Request("https://mariananghel05.github.io/TvApp/Production/src/index.html", "get", "text")
 http.get(text=>{
-    var html = document.createElement('div')
+    var html = document.createElement("div")
     html.innerHTML = text
     body.appendChild(html)
 })
