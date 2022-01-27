fab378c3682c86f173cca71eaef44a73bf9d6613
Marian05 <mariananghel99@gmail.com>
2022-01-27 02:15:43 +0200

    auto push

--- Production/main.js
+++ Production/main.js
@@ -1,21 +1,3 @@
-class HttpRequest{
-    constructor(url, method="GET", responseType="text"){
-        this.url = url;
-        this.method = method;
-        this.responseType = responseType;
-        this.xhr = new XMLHttpRequest();
-    }
-
-    get(implementation){
-        this.xhr.open(this.method, this.url, true);
-        this.xhr.responseType = this.responseType;
-        this.xhr.onload = ()=>{
-            implementation(this.xhr.response);
-        }
-        this.xhr.send();
-    }
-}
-
 var script = document.createElement("script")
 var body = document.body
 
@@ -34,7 +16,7 @@ style.rel = 'stylesheet'
 style.href = 'https://mariananghel05.github.io/TvApp/Production/src/style.css'
 document.head.appendChild(style)
 
-var http = new Request("https://mariananghel05.github.io/TvApp/Production/src/index.html", "GET")
+var http = new HttpRequest("https://mariananghel05.github.io/TvApp/Production/src/index.html", "GET")
 http.get(text=>{
     var html = document.createElement("div")
     html.innerHTML = text
