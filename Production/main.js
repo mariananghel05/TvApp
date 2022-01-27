ba64cbc8741aca4672001fa67c6a29b18a08ebe3
Marian05 <mariananghel99@gmail.com>
2022-01-27 02:27:26 +0200

    auto push

--- Production/main.js
+++ Production/main.js
@@ -1,3 +1,21 @@
+class HttpRequest{
+    constructor(url, method="GET", responseType="text"){
+        this.url = url;
+        this.method = method;
+        this.responseType = responseType;
+        this.xhr = new XMLHttpRequest();
+    }
+
+    get(implementation){
+        this.xhr.open(this.method, this.url, true);
+        this.xhr.responseType = this.responseType;
+        this.xhr.onload = ()=>{
+            implementation(this.xhr.response);
+        }
+        this.xhr.send();
+    }
+}
+
 var script = document.createElement("script")
 var body = document.body
 
