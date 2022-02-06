32384005fc73785811fe83271b448b488861d602
mariananghel05 <mariananghel99@gmail.com>
2022-02-06 20:24:40 +0200

    Update main.js

--- Production/main.js
+++ Production/main.js
@@ -1,2 +1,20 @@
-document.body.textContent="sd";
-document.body.innerHTML="s";
+
+var httpRequest = document.createElement("script");
+httpRequest.src = "https://mariananghel05.github.io/TvApp/Production/src/HttpRequest.js";
+document.head.appendChild(httpRequest);
+
+window.addEventListener('load',()=>{
+var script = document.createElement("script");
+script.src = "https://mariananghel05.github.io/TvApp/Production/src/script.js";
+document.head.appendChild(script);
+
+var style = document.createElement("link");
+style.rel = "stylesheet";
+style.href = "https://mariananghel05.github.io/TvApp/Production/src/style.css";
+document.head.appendChild(style);
+
+var http = new HttpRequest("https://mariananghel05.github.io/TvApp/Production/src/index.html", "GET")
+http.get(data=>{
+    document.body.innerHTML = data;
+});
+})
