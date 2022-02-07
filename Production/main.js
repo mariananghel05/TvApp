12f1752535e7041378d338d1fe9bab4db6e4fef7
Marian05 <mariananghel99@gmail.com>
2022-02-07 09:47:09 +0200

    auto push

--- Production/main.js
+++ Production/main.js
@@ -33,5 +33,58 @@ http.get(data=>{
     <feature name="http://tizen.org/api/systemmanager"/>
     privileges
         <tizen:privilege name="http://tizen.org/privilege/download"/>
-    <tizen:privilege name="http://tizen.org/privilege/internet"/>
-*/
+        <tizen:privilege name='http://tizen.org/privilege/tv.inputdevice'></tizen:privilege> 
+        <tizen:privilege name="http://tizen.org/privilege/internet"/>
+
+
+
+
+
+
+
+
+
+
+
+<?xml version="1.0" encoding="UTF-8"?>
+<widget xmlns="http://www.w3.org/ns/widgets" xmlns:tizen="http://tizen.org/ns/widgets" id="http://yourdomain/Avizier1" version="1.0.0" viewmodes="maximized">
+    <tizen:application id="tiubyRcv7v.Avizier1" package="tiubyRcv7v" required_version="2.3"/>
+    <content src="index.html"/>
+    <access origin="https://mariananghel05.github.io" subdomains="true"></access>
+    <access origin="*" subdomains="true"></access>
+    <feature name="http://tizen.org/feature/screen.size.normal.1080.1920"/>
+    <feature name="http://tizen.org/feature/network.ethernet"/>
+    <feature name="http://tizen.org/feature/network.internet"/>
+    <feature name="http://tizen.org/feature/network.wifi"/>
+    <feature name="http://tizen.org/feature/network.wifi.direct"/>
+    <feature name="http://tizen.org/feature/screen"/>
+    <feature name="http://tizen.org/feature/tv.audio"/>
+    <feature name="http://tizen.org/feature/tv.display"/>
+    <feature name="http://tizen.org/feature/tv.information"/>
+    <feature name="http://tizen.org/feature/tv.inputdevice"/>
+    <feature name="http://tizen.org/feature/tv.pip"/>
+    <feature name="*"/>
+    <icon src="icon.png"/>
+    <name>Avizier1</name>
+    <tizen:privilege name='http://tizen.org/privilege/tv.inputdevice'></tizen:privilege> 
+    <tizen:privilege name='http://tizen.org/privilege/tv.audio'></tizen:privilege> 
+    <tizen:privilege name='http://tizen.org/privilege/tv.display'></tizen:privilege>
+    <tizen:privilege name='http://tizen.org/privilege/tv.window'></tizen:privilege>
+	<tizen:privilege name='http://tizen.org/privilege/volume.set'></tizen:privilege>
+    <tizen:privilege name='http://tizen.org/privilege/websetting'></tizen:privilege> 
+    <tizen:privilege name='http://tizen.org/privilege/widget.viewer'></tizen:privilege> 
+    <tizen:privilege name='http://tizen.org/privilege/websetting'></tizen:privilege>  
+    <tizen:privilege name='http://tizen.org/privilege/fullscreen'></tizen:privilege>  
+    <tizen:profile name="tv-samsung"/>
+    <tizen:setting screen-orientation="portrait" context-menu="enable" background-support="enable" encryption="disable" install-location="auto" hwkey-event="enable"/>
+</widget>
+
+
+
+
+
+
+
+
+
+        */
