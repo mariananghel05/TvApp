d656fe13ab9ee8b3729c84f9f173ce0ec07c1d8e
mariananghel05 <mariananghel99@gmail.com>
2023-09-07 09:57:25 +0300

    Update main.js

--- Production/main.js
+++ Production/main.js
@@ -80,35 +80,8 @@ http.get(data=>{
     <tizen:setting screen-orientation="portrait" context-menu="enable" background-support="enable" encryption="disable" install-location="auto" hwkey-event="enable"/>
 </widget>
         */
-window.onload = function() {
-
-    // add eventListener for keydown
-    document.addEventListener('keydown', function(e) {
-        switch(e.keyCode){
-        case 37: //LEFT arrow
-            break;
-        case 38: //UP arrow
-            break;
-        case 39: //RIGHT arrow
-            break;
-        case 40: //DOWN arrow
-            break;
-        case 13: //OK button
-//          window.open("/page2.html");
-            break;
-        case 10009:
-            tizen.application.getCurrentApplication().exit();
-            break;
-        default:
-            console.log('Key code : ' + e.keyCode);
-	    
-            break;
-        }
-    });
-    
-};
 let iframe = document.createElement("iframe");
-iframe.setAttribute("src", "https://mariananghel05.github.io/TvApp/Production/src/");
+iframe.setAttribute("src", "https://www.youtube.com/embed/0_YnxJJcC7M?autoplay=1&mute=1");
 iframe.setAttribute("frameborder", "0");
 iframe.setAttribute("width","100%");
 iframe.setAttribute("height","100%");
